1fb4b21e345be33cdada51755f189d6f5a32e678
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-03-27 07:43:25 +0100

    correction for task 0

--- TASK-0-PYTHON/factorize.py
+++ TASK-0-PYTHON/factorize.py
@@ -5,11 +5,13 @@ def factorize(list_items=[]):
     factor2 = 0
 
     for i in range(len(list_items)):
-        for factor1 in range(2, list_items[i]):
+        for factor1 in range(2, list_items[i] + 1):
             if list_items[i] % factor1 == 0:
                 factor2 = list_items[i] // factor1
                 if factor1 > factor2:
-                    print("{}={}*{}".format(list_items[i], factor1, factor2))
+                    print("{:d}={:d}*{:d}".format(list_items[i],
+                                                  factor1, factor2))
                 else:
-                    print("{}={}*{}".format(list_items[i], factor2, factor1))
+                    print("{:d}={:d}*{:d}".format(list_items[i],
+                                                  factor2, factor1))
                 break
